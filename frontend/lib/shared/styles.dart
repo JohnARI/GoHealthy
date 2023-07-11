72828396abede01b5060d87257c4ebe35285b046
Jaris <john.aristosa@hotmail.com>
2023-07-11 10:06:59 +0200

    feat(style): created button style following the mockup (#35)

--- frontend/lib/shared/styles.dart
+++ frontend/lib/shared/styles.dart
@@ -1,3 +1,9 @@
 class AppStyle {
-  
+  static const double HORIZONTAL_PADDING = 24.0;
+  static const double BUTTON_HORIZONTAL_PADDING = 25.0;
+  static const double BUTTON_VERTICAL_PADDING = 15.0;
+  static const double TAG_HORIZONTAL_PADDING = BUTTON_HORIZONTAL_PADDING;
+  static const double TAG_VERTICAL_PADDING = BUTTON_VERTICAL_PADDING;
+  static const double BUTTON_RADIUS = 6.0;
+  static const double TAG_RADIUS = 10.0;
 }
